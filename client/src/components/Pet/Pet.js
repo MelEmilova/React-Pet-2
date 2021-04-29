8fb070b966e47b18f21b6dea44bac9cbb6bc6bb7
MelEmilova <edxa77@gmail.com>
2021-04-29 11:51:31 +0300

    likes Action

--- client/src/components/Pet/Pet.js
+++ client/src/components/Pet/Pet.js
@@ -1,7 +1,7 @@
 import {Link} from 'react-router-dom';
 
 const Pet = (props) => {
-  let id = props.pet.id
+  let id = props.pet.id;
   return(
     <li className="otherPet">
       <h3>Name: {props.pet.name}</h3>
@@ -9,10 +9,9 @@ const Pet = (props) => {
        <p className="img"><img src={props.pet.imageURL} alt=""/></p>
       <p className="description">{props.pet.description}</p>
        <div className="pet-info">
-         <Link to="/"><button className="button"><i className="fas fa-heart"></i> Pet</button></Link>
+         {/* <Link to="/"><button className="button"><i className="fas fa-heart"></i> Pet</button></Link> */}
          
         <Link to={`/pets/details/${id}`}><button className="button">Details</button></Link>
-        <i className="fas fa-heart"></i> <span> 2</span>
       </div>
     </li>
   );
